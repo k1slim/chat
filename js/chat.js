bb0ddd90694d69955dee9031f7583de2f52c58fd
k1slim <k1slim@yanex.ru>
2015-07-12 16:00:51 +0300

    first version

--- js/chat.js
+++ js/chat.js
@@ -17,7 +17,8 @@ define(['jquery', 'io', 'handlebars', 'text!../template/messageTemplate.hbs'],
 
                 var self=this;
 
-                this.socket = io.connect('http://chat-k1slim.rhcloud.com');
+                //this.socket = io.connect('http://chat-k1slim.rhcloud.com');
+                this.socket = io();
 
                             //Listeners
                 this.socket.on('message', function(data){
