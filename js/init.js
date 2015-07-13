408112f5310c3e064244b624ca8411a782b0f361
k1slim <k1slim@yanex.ru>
2015-07-13 13:40:13 +0300

    first version

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@ require.config({
     paths: {
         'jquery': '../bower_components/jquery/dist/jquery.min',
         'jquery-ui': '../bower_components/jquery-ui/jquery-ui.min',
-        'io': 'https://cdn.socket.io/socket.io-1.3.5',
+        'io': 'http://chat-k1slim.rhcloud.com:8000/socket.io/socket.io.js',
         'handlebars': '../bower_components/handlebars/handlebars.min',
         'text': '../bower_components/requirejs-text/text',
 
