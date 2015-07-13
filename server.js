f977c0e70b013fe63ee75676b89f2c92a7b9f60f
k1slim <k1slim@yanex.ru>
2015-07-13 12:50:14 +0300

    first version

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.listen(server_port, server_ip_address, function(){
 });
 
 app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', "http://"+req.headers.host+':8000');
+        res.setHeader('Access-Control-Allow-Origin', "*");
 
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
